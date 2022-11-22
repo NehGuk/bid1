d3131801e7ac6ded2f90287ba4a8ab273dcb42c7
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2022-11-22 19:53:38 +0100

    Adds gallery draft

---
+++ src/scripts/item-page/gallery.mjs
@@ -0,0 +1,7 @@
+const productGallery = document.querySelector("#product-gallery");
+const imageCover = document.querySelector("#image-cover");
+
+const imageThumb1 = document.querySelector("#image-thumb-1");
+const imageThumb2 = document.querySelector("#image-thumb-2");
+const imageThumb3 = document.querySelector("#image-thumb-3");
+
