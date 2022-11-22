257f931e7ff9366fe4b1cd4620b88cf7de7c1aa1
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2022-11-22 20:07:40 +0100

    Adds media gallery thumbnails

--- src/scripts/item-page/gallery.mjs
+++ src/scripts/item-page/gallery.mjs
@@ -5,3 +5,21 @@ const imageThumb1 = document.querySelector("#image-thumb-1");
 const imageThumb2 = document.querySelector("#image-thumb-2");
 const imageThumb3 = document.querySelector("#image-thumb-3");
 
+
+imageThumb1.addEventListener("click", () => {
+    imageCover.src = `
+        /assets/sample-image.png
+    `;
+});
+
+imageThumb2.addEventListener("click", () => {
+    imageCover.src = `
+        /assets/sample-image-green.png
+    `;
+});
+
+imageThumb3.addEventListener("click", () => {
+    imageCover.src = `
+        /assets/sample-image-blue.png
+    `;
+});
