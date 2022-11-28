8b3218b3bf16690321c7a582320f370bb1cc2283
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2022-11-28 18:26:36 +0100

    Adds signup script

--- src/scripts/api/api-base-urls.mjs
+++ src/scripts/api/api-base-urls.mjs
@@ -5,3 +5,4 @@ export const API_BASE_URL = `https://api.noroff.dev/api/v1`;
 export const allListingsLoggedOutURL = `${API_BASE_URL}/auction/listings?_seller=true&_bids=true`;
 
 // Register user
+export const registerUserURL = `${API_BASE_URL}/auction/auth/register`;
