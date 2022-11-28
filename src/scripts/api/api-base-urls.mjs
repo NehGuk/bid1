9e7a3e6cdce5352b3029c84b63a0ba4ee0d1667f
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2022-11-28 18:21:54 +0100

    Updated file name

--- src/scripts/api/api-base-urls.mjs
+++ src/scripts/api/api-base-urls.mjs
@@ -3,3 +3,6 @@ export const API_BASE_URL = `https://api.noroff.dev/api/v1`;
 
 // GET ALL LISTINGS LOGGED OUT
 export const allListingsLoggedOutURL = `${API_BASE_URL}/auction/listings?_seller=true&_bids=true`;
+
+// Register user
+export const 
