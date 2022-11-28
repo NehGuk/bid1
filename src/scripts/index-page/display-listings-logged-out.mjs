9757f95dc55fd89bd8de768bdf5093add338f782
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2022-11-28 17:53:46 +0100

    Aligns button and image

--- src/scripts/index-page/display-listings-logged-out.mjs
+++ src/scripts/index-page/display-listings-logged-out.mjs
@@ -15,14 +15,17 @@ export function displayListingsLoggedOut(allListings) {
     listingsContainer.innerHTML += `
         <div class="col">
               <div class="card h-100">
-                <img src="${allListings[i].media[0]}" class="card-img-top" alt="Image description">
+                <img src="${allListings[i].media[0]}" class="card-img-top" style="height: 20rem; object-fit: cover" alt="Image caption: ${allListings[i].title}">
                 <div class="card-body">
                   <h3 class="card-title">${allListings[i].title}</h3>
                   <p class="card-text">${allListings[i].description}</p>
                   <p><strong>Bids: </strong>${allListings[i].bids.length}</p>
                   <p><strong>Ends at: </strong>${allListings[i].endsAt}</p>
-                  <a href="/signup.html"><button class="btn btn-primary px-4">Bid now</button></a>
+                  
                 </div>
+                <div class="card-footer bg-light border-0 pt-0 mt-0">
+                  <a href="/signup.html"><button class="btn btn-primary px-4">Bid now</button></a>
+                  </div>
               </div>
             </div>
         `;
