92dfba89b6de7f5b4a20ab41cfe89d58f6a3ff82
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2022-11-28 16:05:04 +0100

    API call plus initial card display

---
+++ src/scripts/index-page/display-listings-logged-out.mjs
@@ -0,0 +1,21 @@
+export function displayListingsLoggedOut(allListings) {
+  const listingsContainer = document.querySelector("#listingsContainer");
+  listingsContainer.innerHTML = ``;
+
+  for (let i = 0; i < allListings.length; i++) {
+    listingsContainer.innerHTML += `
+        <div class="col">
+              <div class="card h-100">
+                <img src="${allListings[i].media[0]}" class="card-img-top" alt="Image description">
+                <div class="card-body">
+                  <h3 class="card-title">${allListings[i].title}</h3>
+                  <p class="card-text">${allListings[i].description}</p>
+                  <p><strong>Bids: </strong>${allListings[i].bids.length}</p>
+                  <p><strong>Ends at: </strong>${allListings[i].endsAt}</p>
+                  <a href="/signup.html"><button class="btn btn-primary px-4">Bid now</button></a>
+                </div>
+              </div>
+            </div>
+        `;
+  }
+}
---
+++ src/scripts/index-page/fetch-listings-logged-out.mjs
@@ -0,0 +1,13 @@
+import { allListingsLoggedOutURL } from "../api/api-base-urls.mjs";
+import { displayListingsLoggedOut } from "./display-listings-logged-out.mjs";
+
+export async function fetchListingsLoggedOut(url) {
+  try {
+    const response = await fetch(allListingsLoggedOutURL);
+    const allListings = await response.json();
+    displayListingsLoggedOut(allListings);
+  } catch (error) {
+    console.log(error);
+  }
+}
+fetchListingsLoggedOut();
