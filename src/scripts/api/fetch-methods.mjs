28f66f90103602187de34513e49a078cf8e9fa9b
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2022-11-28 22:42:52 +0100

    Sets up register variables

---
+++ src/scripts/api/fetch-methods.mjs
@@ -0,0 +1,5 @@
+export const fetchMetdhods = {
+  registerUser: {},
+  anotherKey: "some value, later an object",
+  yetAnotherKey: "another value, later an object",
+};
--- src/scripts/sign-up-page/register-user.mjs
+++ src/scripts/sign-up-page/register-user.mjs
@@ -1,1 +1,5 @@
-
+import { registerUserURL } from "../api/api-base-urls.mjs";
+import { fetchMetdhods } from "../api/fetch-methods.mjs";
+const { registerUser } = fetchMetdhods;
+console.log(registerUser);
+console.log(registerUserURL);
