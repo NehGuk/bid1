7b534d7e9eed406ec576bfce76d15e5665903427
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2022-11-30 21:33:17 +0100

    Updates functions to display user avatar and data

--- src/scripts/my-account-page/display-profile-info.mjs
+++ src/scripts/my-account-page/display-profile-info.mjs
@@ -3,12 +3,28 @@ const userAvatar = localStorage.getItem("avatar");
 const userEmail = localStorage.getItem("email");
 const userCredits = localStorage.getItem("credits");
 
-const userAvatarContainer = document.querySelector("#user-avatar-container");
-userAvatarContainer.innerHTML = `<img src="${userAvatar}" class="rounded-circle border border-dark" style="width: 200px; height: 200px; object-fit: cover; border-width: 4px !important;" id="profile-avatar" alt=""></img>;`;
+function showUserAvatar() {
+  if (userAvatar === "null") {
+    console.log("No user avatar?");
+    const userAvatarContainer = document.querySelector(
+      "#user-avatar-container"
+    );
+    userAvatarContainer.innerHTML = `<img src="/assets/sample-image.png" class="rounded-circle border border-dark" style="width: 200px; height: 200px; object-fit: cover; border-width: 4px !important;" id="profile-avatar" alt=""></img>`;
+  } else {
+    const userAvatarContainer = document.querySelector(
+      "#user-avatar-container"
+    );
+    userAvatarContainer.innerHTML = `<img src="${userAvatar}" class="rounded-circle border border-dark" style="width: 200px; height: 200px; object-fit: cover; border-width: 4px !important;" id="profile-avatar" alt=""></img>`;
+  }
+}
+showUserAvatar();
 
-const userProfileInfo = document.querySelector("#user-profile-info");
-userProfileInfo.innerHTML = `
+function showUserInfo() {
+  const userProfileInfo = document.querySelector("#user-profile-info");
+  userProfileInfo.innerHTML = `
     <p class="text-center" id="user-name"><b>Name: </b>${userName}</p>
     <p class="text-center" id="user-email"><b>Email: </b>${userEmail}</p>  
-    <p class="text-center" id="user-credits"><b>Credits: </b>1000</p>  
+    <p class="text-center" id="user-credits"><b>Credits: </b>${userCredits}</p>  
 `;
+}
+showUserInfo();
