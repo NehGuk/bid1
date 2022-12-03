6c589943657d366848b66fa9524e4f2609b26590
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2022-12-04 00:09:10 +0100

    Adds query parameters to item page

--- src/scripts/item-page/item-api-call.mjs
+++ src/scripts/item-page/item-api-call.mjs
@@ -1,14 +1,11 @@
 // Get the item ID via the query string from the previous page
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const itemID = urlParams.get("id");
 
 // import the URL for the API call
 import { getListingURL } from "../api/api-base-urls.mjs";
-const listingURL = getListingURL("eb721aab-149d-4ee9-bab4-a298c7713794");
-
-// no image "960bb61f-f6e1-491b-9510-dfd70c0db199"
-// 2 images "07b46cb6-c750-42ac-b2a3-dee8b3ed7139"
-// 3 images "fe677260-8522-43b4-b19f-e76891ef53dc"
-// with more bids and two images a43ef005-8509-4936-908a-4febffb49400
-// with a lot of bids eb721aab-149d-4ee9-bab4-a298c7713794
+const listingURL = getListingURL(itemID);
 
 // import the fetch option method
 import { fetchMetdhods } from "../api/fetch-methods.mjs";
