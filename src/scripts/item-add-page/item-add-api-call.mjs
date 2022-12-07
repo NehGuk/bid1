283ab103317d65abaf1487f0f6028b0add60120c
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2022-12-07 18:03:14 +0100

    Fixes add item function and error behaviour

--- src/scripts/item-add-page/item-add-api-call.mjs
+++ src/scripts/item-add-page/item-add-api-call.mjs
@@ -3,40 +3,47 @@ import { fetchMetdhods } from "../api/fetch-methods.mjs";
 const { createEntry } = fetchMetdhods;
 
 const addNewItemForm = document.querySelector("#add-new-item-form");
-const title = document.querySelector("#title");
-const description = document.querySelector("#description");
-const date = document.querySelector("#date");
-const time = document.querySelector("#time");
-const endsAt = `${date.value}T${time.value}`;
-const media1 = document.querySelector("#media1");
-const media2 = document.querySelector("#media2");
-const media3 = document.querySelector("#media3");
 const errorMessage = document.querySelector("#error-message");
 errorMessage.style.display = "none";
-
-const itemObject = {
-  title: `${title.value}`,
-  description: `${description.value}`,
-  tags: [],
-  media: [`${media1.value}`, `${media2.value}`, `${media3.value}`],
-  endsAt: endsAt,
-};
-
-createEntry.body = JSON.stringify(itemObject);
 
 addNewItemForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
+  const title = document.querySelector("#title");
+  const description = document.querySelector("#description");
+  const date = document.querySelector("#date");
+  const time = document.querySelector("#time");
+  const endsAt = `${date.value}T${time.value}`;
+  const media1 = document.querySelector("#media1");
+  const media2 = document.querySelector("#media2");
+  const media3 = document.querySelector("#media3");
+
+  const itemObject = {
+    title: `${title.value}`,
+    description: `${description.value}`,
+    tags: [],
+    media: [`${media1.value}`, `${media2.value}`, `${media3.value}`],
+    endsAt: endsAt,
+  };
+
+  createEntry.body = JSON.stringify(itemObject);
+
   async function createNewIteminAPI() {
     const response = await fetch(createEntryURL, createEntry);
     const json = await response.json();
-    console.log(response.status);
-    if (response.status !== 201) {
+    if (response.status === 201) {
+      location.href = "/my-account.html";
+    } else {
       errorMessage.style.display = "block";
-      errorMessage.innerHTML = `${json.errors[0].message}.`;
+      errorMessage.innerHTML = ``;
+
+      for (let i = 0; i < json.errors.length; i++) {
+        console.log(i);
+        console.log(json.errors[i].message);
+        errorMessage.style.display = "block";
+        errorMessage.innerHTML += `${json.errors[i].message}<br>`;
+      }
     }
   }
   createNewIteminAPI();
-
-  // Reset form and redirect user to my account page
 });
