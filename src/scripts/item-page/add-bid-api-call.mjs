5c196dec9673dcaf0fc7e83c3ae51b3e4e17a4ef
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2022-12-08 13:46:41 +0100

    Adds error message after bid if necessary

--- src/scripts/item-page/add-bid-api-call.mjs
+++ src/scripts/item-page/add-bid-api-call.mjs
@@ -1,5 +1,11 @@
 export async function sendBidToAPI(url, method) {
   const response = await fetch(url, method);
   const json = await response.json();
-  location.reload();
+  if (response.status === 200) {
+    location.reload();
+  } else {
+    const errorMessage = document.querySelector("#error-message");
+    errorMessage.style.display = "block";
+    errorMessage.innerText = `${json.errors[0].message}`;
+  }
 }
