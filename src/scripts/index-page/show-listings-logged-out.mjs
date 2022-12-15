a5df83a5ac614be008aa14ce684212d9dd148eee
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2022-12-15 15:36:04 +0100

    Adds same API call URL for users logged in or out

--- src/scripts/index-page/show-listings-logged-out.mjs
+++ src/scripts/index-page/show-listings-logged-out.mjs
@@ -1,12 +1,12 @@
 import { clearLocalStorage } from "../clear-local-storage.mjs";
 clearLocalStorage();
 
-import { allListingsLoggedOutURL } from "../api/api-base-urls.mjs";
+import { allListingsDESC } from "../api/api-base-urls.mjs";
 import { displayListingsLoggedOut } from "./display-listings-logged-out.mjs";
 
 export async function fetchListingsLoggedOut(url) {
   try {
-    const response = await fetch(allListingsLoggedOutURL);
+    const response = await fetch(allListingsDESC);
     const allListings = await response.json();
     displayListingsLoggedOut(allListings);
   } catch (error) {
