24fae78070532cde21a2ebf11cc6af02333d0376
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2022-12-16 11:35:41 +0100

    Adds filter buttons asc and desc to home

--- src/scripts/home-page/filters-home.mjs
+++ src/scripts/home-page/filters-home.mjs
@@ -1,21 +1,22 @@
 import { allListingsDESC } from "../api/api-base-urls.mjs";
 import { allListingsASC } from "../api/api-base-urls.mjs";
 
-import { fetchListingsLoggedOut } from "./index-api-call.mjs";
+import { fetchListings } from "./home-api-call.mjs";
 
 const newestFirstButton = document.querySelector("#newest-first-button");
 const oldestFirstButton = document.querySelector("#oldest-first-button");
 
 newestFirstButton.addEventListener("click", () => {
+  console.log("Clickinnnngggg newest first");
   newestFirstButton.classList.add("active");
   oldestFirstButton.classList.remove("active");
   console.log(allListingsDESC);
-  fetchListingsLoggedOut(allListingsDESC);
+  fetchListings(allListingsDESC);
 });
 
 oldestFirstButton.addEventListener("click", () => {
   oldestFirstButton.classList.add("active");
   newestFirstButton.classList.remove("active");
   console.log(allListingsASC);
-  fetchListingsLoggedOut(allListingsASC);
+  fetchListings(allListingsASC);
 });
