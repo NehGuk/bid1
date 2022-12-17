6093eb17aee43068285ca3d84096f9399a0292e9
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2022-12-17 16:51:31 +0100

    Redesigns editor's picks section

--- src/scripts/home-page/home-api-call.mjs
+++ src/scripts/home-page/home-api-call.mjs
@@ -1,6 +1,6 @@
 import { allListingsDESC } from "../api/api-base-urls.mjs";
 import { fetchMetdhods } from "../api/fetch-methods.mjs";
-import { showEditorPick } from "./editors-pick.mjs";
+import { showEditorPicks } from "./editors-pick.mjs";
 const { getListing } = fetchMetdhods;
 
 import { displayListingsHome } from "./display-listings.mjs";
@@ -13,7 +13,7 @@ export async function fetchListings(url, method) {
     const response = await fetch(url, getListing);
     const allListings = await response.json();
     displayListingsHome(allListings);
-    showEditorPick(allListings);
+    showEditorPicks(allListings);
     searchListingsHome(allListings);
     clearSearchFieldHome(allListings);
   } catch (error) {
