932726fa03129c4240b2006bf7b62139014a90d6
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2022-12-17 17:30:06 +0100

    Editors choice slider ready and working

--- src/scripts/home-page/editors-pick.mjs
+++ src/scripts/home-page/editors-pick.mjs
@@ -1,3 +1,26 @@
-export function showEditorPicks(allListings) {
-  console.log("Gettin showEditorPicks function");
+export function showEditorPicks(editorsChoiceArray) {
+  console.log("Getting showEditorPicks function");
+  console.log(editorsChoiceArray);
+
+  const editorsChoideSlider = document.querySelector("#editors-choices-slider");
+  const slideImage = document.querySelector("#slide-image");
+  const slideTitle = document.querySelector("#slide-title");
+
+  editorsChoideSlider.innerHTML = ``;
+  for (let i = 0; i < editorsChoiceArray.length; i++) {
+    console.log(editorsChoiceArray[i].id);
+
+    editorsChoideSlider.innerHTML += `
+      <div class="carousel-item active">
+        <img src="${editorsChoiceArray[i].media[0]}" width="100%" height="400px" style="object-fit:cover; -webkit-filter: blur(5px); filter: brightness(0.5) blur(5px)" id="slide-image">
+        <div class="container">
+          <div class="carousel-caption text-center">
+            <p class="small text-white">EDITOR'S CHOICE</p>
+            <h3 id="slide-title">${editorsChoiceArray[i].title}</h3>    
+            <p><a class="btn btn-lg btn-primary" href="/item.html?id=${editorsChoiceArray[i].id}">Bid now</a></p>
+          </div>
+        </div>
+      </div>
+    `;
+  }
 }
--- src/scripts/home-page/home-api-call.mjs
+++ src/scripts/home-page/home-api-call.mjs
@@ -1,10 +1,9 @@
 import { allListingsDESC } from "../api/api-base-urls.mjs";
 import { fetchMetdhods } from "../api/fetch-methods.mjs";
-import { showEditorPicks } from "./editors-pick.mjs";
 const { getListing } = fetchMetdhods;
-
+import { showEditorPicks } from "./editors-pick.mjs";
+import { editorsChoiceArray } from "./editors-pick-array.mjs";
 import { displayListingsHome } from "./display-listings.mjs";
-
 import { searchListingsHome } from "./search-bar.mjs";
 import { clearSearchFieldHome } from "./search-bar.mjs";
 
@@ -12,8 +11,8 @@ export async function fetchListings(url, method) {
   try {
     const response = await fetch(url, getListing);
     const allListings = await response.json();
+    showEditorPicks(editorsChoiceArray);
     displayListingsHome(allListings);
-    showEditorPicks(allListings);
     searchListingsHome(allListings);
     clearSearchFieldHome(allListings);
   } catch (error) {
